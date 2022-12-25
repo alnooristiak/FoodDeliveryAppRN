265070d528553bb4349d64e5abc979d7e0cfbb39
Al Noor Istiak Mahmud <istiakmahmud1010@gmail.com>
2022-12-25 22:06:00 +0600

    complited sign in screen

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import SignInScreen from './src/screens/SignInScreen';
 import WelcomeScreen from './src/screens/WelcomeScreen';
 import { spacing } from './src/theme/spacing';
 
@@ -7,8 +8,9 @@ export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar backgroundColor = "#00BCD4"/>
-      <Text>Open up App.js to start working on your app!</Text>
-      <WelcomeScreen />
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
+      {/* <WelcomeScreen /> */}
+      <SignInScreen />
     </View>
   );
 }
---
+++ src/screens/SignInScreen.js
@@ -0,0 +1,97 @@
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
+import { colors } from '../theme/colors'
+import { spacing } from '../theme/spacing'
+import { AntDesign } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+
+const SignInScreen = () => {
+
+    const [email, setEmail] = useState(false);
+    const [password, setPassword] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.signInText}>Sign In</Text>
+      <View style={styles.inputContainer}>
+        <AntDesign name="user" size={24} color="black" />
+        <TextInput style={styles.inputSec} 
+        onFocus={() => {
+            setEmail(true)
+            setPassword(false)
+            }} placeholder='Enter Email' />
+      </View>
+      <View style={styles.inputContainer}>
+        <FontAwesome name="eye-slash" size={24} color="black" />
+        <TextInput secureTextEntry={true} style={styles.inputSec} placeholder='Password' />
+      </View>
+      {/* btn section */}
+      <View>
+        <TouchableOpacity style={styles.btn}>
+            <Text style={styles.btnText}>Sign In</Text>
+        </TouchableOpacity>
+      </View>
+      {/* forgote password sec */}
+      <View style={styles.optionalText}>
+        <Text style={{fontSize: spacing[8], marginTop: spacing[5]}}>Forgote Password</Text>
+        <Text style={{fontSize: spacing[8], marginTop: spacing[5]}}>OR</Text>
+        <Text style={{fontSize: spacing[8], marginTop: spacing[5]}}>Sign in With</Text>
+      </View>
+      {/* sign in with sec */}
+      <View style={styles.icons}>
+        <TouchableOpacity style={{margin: spacing[4]}}>
+            <FontAwesome name="facebook-f" size={24} color="orange" />
+        </TouchableOpacity>
+
+        <TouchableOpacity style={{margin: spacing[4]}}>
+            <FontAwesome name="google" size={24} color="orange" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
+
+export default SignInScreen
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      inputContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center'
+      },
+      signInText: {
+        fontSize: spacing[10],
+        color: colors.orange,
+      },
+      inputSec: {
+        borderColor: colors.orange,
+        borderWidth: 1,
+        padding: spacing[3],
+        marginTop: spacing[5],
+        width: '80%',
+      },
+      optionalText: {
+        alignItems: 'center'
+      },
+      btn: {
+        marginTop: spacing[10],
+        backgroundColor: colors.greay,
+        margin: spacing[5],
+        padding: spacing[4],
+        borderRadius: spacing[2],
+    },
+    btnText: {
+        fontSize: spacing[8]
+    },
+    icons: {
+        marginTop: spacing[5],
+        flexDirection:'row',
+    }
+})
